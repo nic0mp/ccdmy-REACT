21e575b6e3dbff7ed8ae14311e2780162bfd0dcb
nic0mp <nico.mp215@gmail.com>
2022-02-27 21:34:27 -0600

    ATTRIBUTES IN JSX

--- Intro to JSX/intro2JSX.js
+++ Intro to JSX/intro2JSX.js
@@ -2,3 +2,7 @@
 
 // JSX element being saved in a variable:
 const myArticle = <article>My article</article>;
+
+// ATTRIBUTES IN JSX
+const p1 = <p id='large'>foo</p>;
+const p2 = <p id='small'>bar</p>;
