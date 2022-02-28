79da6b2e698db4f8d7175a912f359816f9db15f6
nic0mp <nico.mp215@gmail.com>
2022-02-27 21:43:42 -0600

    NESTED JSX

--- Intro to JSX/intro2JSX.js
+++ Intro to JSX/intro2JSX.js
@@ -1,8 +1,15 @@
-<p>Hello world</p>
+<p>Hello world</p>;
 
 // JSX element being saved in a variable:
 const myArticle = <article>My article</article>;
 
 // ATTRIBUTES IN JSX
-const p1 = <p id='large'>foo</p>;
-const p2 = <p id='small'>bar</p>;
+const p1 = <p id="large">foo</p>;
+const p2 = <p id="small">bar</p>;
+
+// NESTED JSX
+const myDiv = (
+  <div>
+    <h1>Hello World</h1>
+  </div>
+);
