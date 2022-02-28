4feeb635041fe5d90ad63d321409fe063dc6dfca
nic0mp <nico.mp215@gmail.com>
2022-02-28 14:10:03 -0600

    ReactDOM.render() I

--- Intro to JSX/intro2JSX.js
+++ Intro to JSX/intro2JSX.js
@@ -33,3 +33,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(<h1>Hello world</h1>, document.getElementById('app'));
+
+// ReactDOM.render() I
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+ReactDOM.render(<h1>Render me!</h1>, document.getElementById('app'));
