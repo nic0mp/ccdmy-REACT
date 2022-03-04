224d8ddc042e6e48a0ce35f34fd633ab263fb920
nic0mp <nico.mp215@gmail.com>
2022-03-04 16:13:20 -0600

    self closing tags

--- Intro to JSX/2-AdvancedJSX.js
+++ Intro to JSX/2-AdvancedJSX.js
@@ -12,3 +12,18 @@ ReactDOM.render(
   myDiv,
   document.getElementById('app')
 )
+
+// SELF CLOSING TAGS
+const profile = (
+    <div>
+        <h1>I AM JENKINS</h1>
+        <img src="images/jenkins.png" />
+        <article>
+        I LIKE TO SIT
+        <br/>
+        JENKINS IS MY NAME
+        <br/>
+        THANKS HA LOT
+        </article>
+    </div>
+);
