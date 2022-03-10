01987e578d986f932739ba1dac6dae7e1a39c3dc
nic0mp <nico.mp215@gmail.com>
2022-03-09 20:26:01 -0600

    update

--- Intro to JSX/2-AdvancedJSX.js
+++ Intro to JSX/2-AdvancedJSX.js
@@ -37,3 +37,36 @@ ReactDOM.render(
   <h1>{2 + 3}</h1>,
   document.getElementById('app')
 ); // OUTPUT: 5 ..
+
+// pi.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const pi = (
+  <div>
+    <h1>
+      PI, YALL!
+    </h1>
+    <p>
+      {Math.PI.toFixed(20)}
+    </p>
+  </div>
+);
+
+ReactDOM.render(
+	pi,
+	document.getElementById('app')
+);
+// app.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const math = (
+  <h1>2 + 3 = {2 + 3}</h1>
+);
+
+ReactDOM.render(
+  math,
+  document.getElementById('app')
+)
