5bfb7f390c97d129480ccaa2856304ea633d4b15
nic0mp <nico.mp215@gmail.com>
2022-03-14 12:55:44 -0500

    update

--- Intro to JSX/2-AdvancedJSX.js
+++ Intro to JSX/2-AdvancedJSX.js
@@ -70,3 +70,11 @@ ReactDOM.render(
   math,
   document.getElementById('app')
 )
+
+// Variables in JSX
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const theBestString = 'tralalalala i am da best';
+
+ReactDOM.render(<h1>{theBestString}</h1>, document.getElementById('app'));
