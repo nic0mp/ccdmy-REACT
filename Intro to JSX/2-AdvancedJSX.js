50d43007a380c34f943a77fc95f9d142da34080c
nic0mp <nico.mp215@gmail.com>
2022-03-15 14:54:21 -0500

    event Listeners

--- Intro to JSX/2-AdvancedJSX.js
+++ Intro to JSX/2-AdvancedJSX.js
@@ -95,3 +95,22 @@ ReactDOM.render(
   gooseImg,
   document.getElementById('app')
 )
+
+// Eent listeners
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function makeDoggy(e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+const kitty = (
+	<img onClick={makeDoggy}
+		src="https://content.codecademy.com/courses/React/react_photo-kitty.jpg" 
+		alt="kitty" />
+);
+
+ReactDOM.render(kitty, document.getElementById('app'));
