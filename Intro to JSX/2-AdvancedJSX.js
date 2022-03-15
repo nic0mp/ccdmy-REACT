ed18bed33c63fecb74e1be3bb69c468b7d5bfd68
nic0mp <nico.mp215@gmail.com>
2022-03-15 12:34:54 -0500

    Variable Attributes in JSX

--- Intro to JSX/2-AdvancedJSX.js
+++ Intro to JSX/2-AdvancedJSX.js
@@ -78,3 +78,20 @@ import ReactDOM from 'react-dom';
 const theBestString = 'tralalalala i am da best';
 
 ReactDOM.render(<h1>{theBestString}</h1>, document.getElementById('app'));
+
+// Variable Attributes in JSX
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const goose = 'https://content.codecademy.com/courses/React/react_photo-goose.jpg';
+
+// Declare new variable here:
+const gooseImg = (
+  <img 
+   src={goose}
+  />
+)
+ReactDOM.render(
+  gooseImg,
+  document.getElementById('app')
+)
