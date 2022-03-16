7de8b3629bb38f49e58c0611570019c066967545
nic0mp <nico.mp215@gmail.com>
2022-03-16 15:59:26 -0500

    app js update

--- Intro to JSX/3-Animal Fun Facts/app.js
+++ Intro to JSX/3-Animal Fun Facts/app.js
@@ -2,16 +2,16 @@ import { animals } from './animals';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const title = '';
 const background = <img 
   className='background'
   alt='ocean'
   src='/images/ocean.jpg'
 />;
-const title = '';
 
 const animalFacts = (
     <div>
-        <h1>{title === '' ? 'Click an animal for a fun fact' : title}</h1>
+        <h1>{title === '' ? 'Click an animal for a fun fact' : title}{background}</h1>
     </div>
   
 )
