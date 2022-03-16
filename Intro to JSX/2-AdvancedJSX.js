7670c6f33de45f2a2a9de24959073b779ea2b96b
nic0mp <nico.mp215@gmail.com>
2022-03-15 21:44:11 -0500

    .map jsx

--- Intro to JSX/2-AdvancedJSX.js
+++ Intro to JSX/2-AdvancedJSX.js
@@ -165,3 +165,20 @@ ReactDOM.render(
 	img, 
 	document.getElementById('app')
 );
+
+// .map in jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleLis = people.map(person =>
+  // expression goes here:
+  <li>{person}</li>
+);
+
+// ReactDOM.render goes here:
+ReactDOM.render(
+  <ul>{peopleLis}</ul>,
+  document.getElementById('app')
+);
