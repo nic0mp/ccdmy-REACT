1acf7fa7c7c7e97f5db9aabfd9ae7bca5ded3521
nic0mp <nico.mp215@gmail.com>
2022-03-16 11:32:32 -0500

    update

--- Intro to JSX/3-AnimalFunFacts.js
+++ Intro to JSX/3-AnimalFunFacts.js
@@ -1,1 +1,12 @@
+import { animals } from './animals';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const title = ' ';
+
+const animalFacts = (
+    <div>
+        <h1></h1>
+    </div>
+  
+)
