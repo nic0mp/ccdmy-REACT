3778a5ee8e231ae34dd9abed15bf7f277179246f
nic0mp <nico.mp215@gmail.com>
2022-03-15 22:15:51 -0500

    keys

--- Intro to JSX/2-AdvancedJSX.js
+++ Intro to JSX/2-AdvancedJSX.js
@@ -182,3 +182,20 @@ ReactDOM.render(
   <ul>{peopleLis}</ul>,
   document.getElementById('app')
 );
+
+// Keys
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleLis = people.map((person, i) =>
+  // expression goes here:
+  <li key={'person_' + i}>{person}</li>
+);
+
+// ReactDOM.render goes here:
+ReactDOM.render(
+  <ul>{peopleLis}</ul>,
+  document.getElementById('app')
+)
