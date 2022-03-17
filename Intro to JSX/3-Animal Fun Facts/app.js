d6a3b07a38705ba392636687045286cb200f58fc
nic0mp <nico.mp215@gmail.com>
2022-03-17 14:25:56 -0500

    update app js

--- Intro to JSX/3-Animal Fun Facts/app.js
+++ Intro to JSX/3-Animal Fun Facts/app.js
@@ -9,6 +9,20 @@ const background = <img
   src='/images/ocean.jpg'
 />;
 
+const images =[];
+for (const animal in animals){
+  images.push(
+    <img 
+      key= {animal}
+      className='animal'
+      alt= {animal}
+      src= {animals[animal].image}
+      aria-label= {animal}
+      role= 'button'
+    />
+  )
+}
+
 const animalFacts = (
     <div>
         <h1>{title === '' ? 'Click an animal for a fun fact' : title}{background}</h1>
